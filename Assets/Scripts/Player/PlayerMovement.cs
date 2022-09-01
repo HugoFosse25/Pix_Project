4e2c8daa0b4f0693f0599cff6e8d92ce95aaf21b
Hugo Fossé <fosse.hugo25@gmail.com>
2022-09-01 22:21:01 +0200

    Update PlayerMovement.cs
    Create a dashing system

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -14,30 +15,45 @@ public class PlayerMovement : MonoBehaviour
     private bool canDash = true;
     private bool isDashing;
 
+    private int playerDirection; // if value = 1 == flipX=false ; -1 = flipX=true
+
     private float horizontalMovement;
     private float characterVelocity;
-    private float dashingPower = 24f;
+    private float dashingPower = 18f;
     private float dashingTime = 0.2f;
     private float dashingCooldown = 1f;
 
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
+    private TrailRenderer tr;
     
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        tr = GetComponent<TrailRenderer>();
     }
 
     private void Update()
     {
+
+        if (isDashing)  //The player cannot move while isDashing
+        {
+            return;
+        }
+
         horizontalMovement = Input.GetAxis("Horizontal");
         
         if (Input.GetButtonDown("Jump"))
         {
             isJumpButtonDown = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
         }
         
     }
@@ -45,7 +61,12 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
-        
+
+        if (isDashing)//The player cannot move while isDashing
+        {
+            return;
+        }
+
         MovePlayer(horizontalMovement * moveSpeed * Time.deltaTime);
 
         characterVelocity = Mathf.Abs(rb.velocity.x); //Keep the velocity positive
@@ -68,16 +89,37 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-        void Flip(float _velocity)
+    void Flip(float _velocity)
     {
-         if(_velocity > 0.05f)
+        if(_velocity > 0.05f)
         {
             spriteRenderer.flipX = false;
+            playerDirection = 1;
         }
         else if(_velocity < -0.05f)
         {
             spriteRenderer.flipX = true;
+            playerDirection = -1;
         }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;   //Remove gravity
+        //Start dashing
+        rb.velocity = new Vector2(playerDirection * dashingPower, 0f);
+        tr.emitting = true; //Enabled the dash effect
+        yield return new WaitForSeconds(dashingTime);
+        //Stop Dashing
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;  //Set original gravity
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 
     private void OnDrawGizmos() //Drawn circle for groundCheck
